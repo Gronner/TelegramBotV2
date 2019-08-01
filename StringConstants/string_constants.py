d40c973028861574e2a71bec892e2fe88e1807f7
Gronner <Gronner@users.noreply.github.com>
2019-08-01 19:19:40 +0200

    Add collector class for constant strings

---
+++ StringConstants/string_constants.py
@@ -0,0 +1,10 @@
+"""
+Contains constants message strings used throughout the telegram bot.
+"""
+
+class EnglishStrings:
+    """
+    Contains all english messages.
+    """
+    UNKOWN_COMMAND = "This command is not available. Type \\help to get a " + \
+                     "list of all available commands!"

1cf13e8973895c2e32f585c8c5cd267346b43a4a
Gronner <Gronner@users.noreply.github.com>
2019-08-01 19:01:21 +0200

    Add skeleton for the bot

---
+++ cabal_bot.py
@@ -0,0 +1,15 @@
+"""
+Runner for the C.A.B.A.L. Telegram Bot.
+"""
+from telegram.ext import Updater
+
+
+def main():
+    """
+    Main runner function for the C.A.B.A.L. Bot.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
